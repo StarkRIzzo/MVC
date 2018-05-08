450f8ab2055832f67295b69f58452a4fd569a226
Stark <ettoreastark@gmail.com>
2018-05-08 11:04:29 -0400

    2

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,12 +5,21 @@
         protected $parameters = [];
         
         public function __construct() {
-            $url = $this->getUrl();
+           $url = $this->getUrl();
+           if(file_exists("../app/controllers/". ucwords($url[0]) . ".php")) {
+               $this->controller = ucwords($url[0]);
+               unset($url[0]);
+           }
+           require_once "../app/controllers/". $this->controller . ".php";
+           $this->controller = new $this->controller;
         }
         
         public function getUrl() {
             if(isset($_GET["url"])) {
-                echo $_GET["url"];
+                $url = rtrim($_GET["url"], "/");
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode("/", $url);
+                return $url;
             }
         }
     }
